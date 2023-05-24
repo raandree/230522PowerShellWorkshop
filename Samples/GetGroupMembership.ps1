d672271985564d698cc7f9cdc0bdd71fdca37c6e
Raimund Andree <r.andree@live.com>
2023-05-24 10:41:56 +0200

    Added write-host

--- Samples/GetGroupMembership.ps1
+++ Samples/GetGroupMembership.ps1
@@ -3,6 +3,6 @@ $wi = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $wi.Groups.Value -contains 'S-1-5-32-544'
 
 $wi.Groups | ForEach-Object {
-    $_.Translate([System.Security.Principal.NTAccount])
+    Write-Host $_.Translate([System.Security.Principal.NTAccount]) -ForegroundColor Green
 }
 
